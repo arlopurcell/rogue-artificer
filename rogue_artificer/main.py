90affaa84114a38319ffa34cd693a14d2a8e39cc
Arlo J Purcell <arlo@decodable.co>
2024-03-27 15:08:59 -0400

    change window title

--- rogue_artificer/main.py
+++ rogue_artificer/main.py
@@ -42,7 +42,7 @@ def main():
         screen_width,
         screen_height,
         tileset=tileset,
-        title="Yet Another Roguelike Tutorial",
+        title="Rogue Artificer",
         vsync=True,
     ) as context:
         root_console = tcod.console.Console(screen_width, screen_height, order="F")
